f22deae6885b3a631ba11b0e2d3513a1ace5e2e8
mackwhite <mwhite@fiu.edu>
2024-01-07 17:38:57 -0500

    did not commit

--- SpatSimMixingModels_2024_MW.R
+++
@@ -1,7 +0,0 @@
-# background --------------------------------------------------------------
-
-#author: Mack White
-#project: SRFCEA - Snook Spatial Similarity Manuscript
-#goal of script: update mixing models through 2023
-#date(s): January 2024
-
